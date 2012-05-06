5fc2beb637d5a20ab7022967d3e25673ac3bc316
Peter Aronoff <telemachus@arpinum.org>
2012-05-06 18:22:56 -0400

    Update driver

--- watchman.rb
+++ watchman.rb
@@ -5,11 +5,23 @@ require "./db/setup"
 Dir.glob('./models/*').each { |r| require r}
 require "./db/seed"
 
-puts "There are #{Show.count} in the database"
+puts "There are #{Author.count} authors in the database."
 
-Network.all.each do |network|
-	puts "Shows airing on #{network}"
-	network.shows.each do |show|
-		puts show
-	end	
+Author.all.each do |a|
+  puts "\t#{a}"
+end
+
+print "Give the last name of the author whose books you want to see: "
+author = gets.chomp
+
+# I'm pretty sure that there's a better way to do this in SQL, but I'm
+# not comfortable enough with SQL, so I'm doing it in Ruby.
+books = Book.all.select { |b| b.author.last_name == author }
+
+if books.count > 0
+  books.each do |b|
+    puts "\t#{b}"
+  end
+else
+  puts "Sorry: no books by #{author}. Please check the names again."
 end
