09b0920a0c92b6883f50b03e0da351452b19ec20
Peter Aronoff <telemachus@arpinum.org>
2012-05-06 18:20:31 -0400

    Remove unnecessary files

--- db/migrate/201205031230_create_shows.rb
+++
@@ -1,9 +0,0 @@
-class CreateShows < ActiveRecord::Migration
-	def change
-		create_table :shows do |t|
-			t.string :name
-			t.string :day_of_week
-			t.integer :hour_of_day
-		end
-	end
-end
--- models/network.rb
+++
@@ -1,7 +0,0 @@
-class Network < ActiveRecord::Base
-	has_many :shows
-
-	def to_s
-		name
-	end
-end
