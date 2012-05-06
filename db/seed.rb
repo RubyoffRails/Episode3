6eb8d2f9b8c8e5cdd3b6726a64a7dd849e580959
Peter Aronoff <telemachus@arpinum.org>
2012-05-06 18:21:37 -0400

    New db configuration

--- db/seed.rb
+++ db/seed.rb
@@ -1,7 +1,150 @@
 # Cleaning Out
-Network.delete_all
-Show.delete_all
-amc = Network.create(name: "AMC")
-nbc = Network.create(name: "NBC")
-Show.create(name: "Mad Men", day_of_week: "Sunday", hour_of_day: 22, network: amc)
-Show.create(name: "Community", day_of_week: "Thursday", hour_of_day: 20, network: nbc)
+Author.delete_all
+
+brust = Author.create({
+  :first_name => 'Steven',
+  :last_name => 'Brust',
+  :birthdate => '11231955'
+})
+
+crais = Author.create({
+  :first_name => 'Robert',
+  :last_name => 'Crais',
+  :birthdate => '06201953'
+})
+
+Book.delete_all
+
+Book.create({
+  :name => 'Jhereg',
+  :author => brust,
+  :publication_year => '1983',
+  :genre => 'fantasy',
+  :series => 'Taltos'
+})
+Book.create({
+  :name => 'Yendi',
+  :author => brust,
+  :publication_year => '1984',
+  :genre => 'fantasy',
+  :series => 'Taltos'
+})
+Book.create({
+  :name => 'Teckla',
+  :author => brust,
+  :publication_year => '1987',
+  :genre => 'fantasy',
+  :series => 'Taltos'
+})
+Book.create({
+  :name => 'Taltos',
+  :author => brust,
+  :publication_year => '1988',
+  :genre => 'fantasy',
+  :series => 'Taltos'
+})
+Book.create({
+  :name => 'Phoenix',
+  :author => brust,
+  :publication_year => '1990',
+  :genre => 'fantasy',
+  :series => 'Taltos'
+})
+Book.create({
+  :name => 'Athyra',
+  :author => brust,
+  :publication_year => '1993',
+  :genre => 'fantasy',
+  :series => 'Taltos'
+})
+Book.create({
+  :name => 'Orca',
+  :author => brust,
+  :publication_year => '1996',
+  :genre => 'fantasy',
+  :series => 'Taltos'
+})
+Book.create({
+  :name => 'Dragon',
+  :author => brust,
+  :publication_year => '1998',
+  :genre => 'fantasy',
+  :series => 'Taltos'
+})
+Book.create({
+  :name => 'Issola',
+  :author => brust,
+  :publication_year => '2001',
+  :genre => 'fantasy',
+  :series => 'Taltos'
+})
+Book.create({
+  :name => 'Dzur',
+  :author => brust,
+  :publication_year => '2006',
+  :genre => 'fantasy',
+  :series => 'Taltos'
+})
+Book.create({
+  :name => 'Jhegaala',
+  :author => brust,
+  :publication_year => '2008',
+  :genre => 'fantasy',
+  :series => 'Taltos'
+})
+Book.create({
+  :name => 'Iorich',
+  :author => brust,
+  :publication_year => '2010',
+  :genre => 'fantasy',
+  :series => 'Taltos'
+})
+Book.create({
+  :name => 'Tiassa',
+  :author => brust,
+  :publication_year => '2011',
+  :genre => 'fantasy',
+  :series => 'Taltos'
+})
+Book.create({
+  :name => 'The Monkey\'s Raincoat',
+  :author => crais,
+  :publication_year => '1987',
+  :genre => 'detective',
+  :series => 'Cole'
+})
+Book.create({
+  :name => 'Stalking the Angel',
+  :author => crais,
+  :publication_year => '1989',
+  :genre => 'detective',
+  :series => 'Cole'
+})
+Book.create({
+  :name => 'Lullaby Town',
+  :author => crais,
+  :publication_year => '1992',
+  :genre => 'detective',
+  :series => 'Cole'
+})
+Book.create({
+  :name => 'Free Fall',
+  :author => crais,
+  :publication_year => '1993',
+  :genre => 'detective',
+  :series => 'Cole'
+})
+Book.create({
+  :name => 'Voodoo River',
+  :author => crais,
+  :publication_year => '1995',
+  :genre => 'detective',
+  :series => 'Cole'
+})
+Book.create({
+  :name => 'Sunset Express',
+  :author => crais,
+  :publication_year => '1996',
+  :genre => 'detective',
+  :series => 'Cole'
+})
--- db/setup.rb
+++ db/setup.rb
@@ -7,9 +7,14 @@ connection_details = YAML::load(File.open('config/database.yml'))
 
 
 # Setup out connection details
-ActiveRecord::Base.establish_connection(connection_details.merge({database: 'postgres', schema_search_path: 'public'}))
-# create the 'tv' database 
-ActiveRecord::Base.connection.create_database(connection_details.fetch(:database)) rescue nil
+ActiveRecord::Base.establish_connection(
+  connection_details.merge({
+    :database => 'postgres',
+    :schema_search_path => 'public'
+  })
+)
+# create the 'books' database
+ActiveRecord::Base.connection.create_database(connection_details['database']) rescue nil
 # connect to it
 ActiveRecord::Base.establish_connection(connection_details)
 # Migrate all the things
