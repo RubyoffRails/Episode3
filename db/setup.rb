a19ff6a1293f380aee9f5ac5614873ceb0e9052e
Jesse Wolgamott <jesse.wolgamott@gmail.com>
2012-05-07 21:33:14 -0500

    use hashrocket syntax

--- db/setup.rb
+++ db/setup.rb
@@ -4,12 +4,11 @@ require 'yaml'
 
 connection_details = YAML::load(File.open('config/database.yml'))
 
-
-
 # Setup out connection details
-ActiveRecord::Base.establish_connection(connection_details.merge({database: 'postgres', schema_search_path: 'public'}))
+ActiveRecord::Base.establish_connection(connection_details.merge({'database'=> 'postgres', 'schema_search_path'=> 'public'}))
 # create the 'tv' database 
-ActiveRecord::Base.connection.create_database(connection_details.fetch(:database)) rescue nil
+ActiveRecord::Base.connection.drop_database (connection_details.fetch('database')) rescue nil
+ActiveRecord::Base.connection.create_database(connection_details.fetch('database')) rescue nil
 # connect to it
 ActiveRecord::Base.establish_connection(connection_details)
 # Migrate all the things
