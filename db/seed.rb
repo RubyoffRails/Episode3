36c83b2768fa81abdd78b861981a522d2849c4f7
Ralphos <wintle.ralph@gmail.com>
2012-05-09 14:53:53 -0500

    Completed Tiger level

--- db/seed.rb
+++ db/seed.rb
@@ -4,4 +4,9 @@ Show.delete_all
 amc = Network.create(name: "AMC")
 nbc = Network.create(name: "NBC")
 Show.create(name: "Mad Men", day_of_week: "Sunday", hour_of_day: 22, network: amc)
-Show.create(name: "Community", day_of_week: "Thursday", hour_of_day: 20, network: nbc)
+Show.create(name: "Community", day_of_week: "Thursday", hour_of_day: 19, network: nbc)
+Show.create(name: "How I met your Mother", day_of_week: "Sunday", hour_of_day: 21, network: amc)
+Show.create(name: "Modern Familiy", day_of_week: "Wednesday", hour_of_day: 23, network: nbc)
+Show.create(name: "Two and a Half Men", day_of_week: "Tuesday", hour_of_day: 22, network: amc)
+Show.create(name: "Friends", day_of_week: "Monday", hour_of_day: 12, network: nbc)
+
--- watchman.rb
+++ watchman.rb
@@ -6,10 +6,21 @@ Dir.glob('./models/*').each { |r| require r}
 require "./db/seed"
 
 puts "There are #{Show.count} in the database"
+puts
+puts "What day do you want to watch shows?"
+puts 
+print '~>'
+day = gets.chomp.capitalize
 
 Network.all.each do |network|
-	puts "Shows airing on #{network}"
-	network.shows.each do |show|
-		puts show
-	end	
+  network.shows.where(day_of_week: day).each do |show|
+    puts show
+  end
 end
+
+# Network.all.each do |network|
+#   puts "Shows airing on #{network}"
+#   network.shows.each do |show|
+#     puts show
+#   end 
+# end
