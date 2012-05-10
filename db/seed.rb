b2e714272b5df938438b02e34e1ac5607fd4baa7
Elliott Cutright <elliott.cutright@gmail.com>
2012-05-09 17:13:06 -0700

    Feature: Added 2 shows and added user input to select day of week for show

--- db/seed.rb
+++ db/seed.rb
@@ -3,5 +3,9 @@ Network.delete_all
 Show.delete_all
 amc = Network.create(name: "AMC")
 nbc = Network.create(name: "NBC")
+discovery = Network.create(name: "Discovery")
+history = Network.create(name: "History")
 Show.create(name: "Mad Men", day_of_week: "Sunday", hour_of_day: 22, network: amc)
 Show.create(name: "Community", day_of_week: "Thursday", hour_of_day: 20, network: nbc)
+Show.create(name: "Mythbusters", day_of_week: "Sunday", hour_of_day: 19, network: discovery)
+Show.create(name: "Top Shot", day_of_week: "Tuesday", hour_of_day: 18, network: history)
--- watchman.rb
+++ watchman.rb
@@ -6,10 +6,12 @@ Dir.glob('./models/*').each { |r| require r}
 require "./db/seed"
 
 puts "There are #{Show.count} in the database"
+puts "What day would you like to watch tv?"
+day = gets.chomp
 
 Network.all.each do |network|
-	puts "Shows airing on #{network}"
-	network.shows.each do |show|
+	network.shows.where(:day_of_week => day).each do |show|
+	  puts "Shows airing on #{network}"
 		puts show
 	end	
 end
