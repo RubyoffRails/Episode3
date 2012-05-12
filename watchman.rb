2ccaf7118e43add8f17b200a61f6ba218c3e5fbe
Alejandro <mr.gaviria@gmail.com>
2012-05-12 01:05:43 -0400

    Completed Tiger Level

--- watchman.rb
+++ watchman.rb
@@ -2,14 +2,22 @@ require 'rubygems'
 require 'bundler/setup'
 
 require "./db/setup"
-Dir.glob('./models/*').each { |r| require r}
+Dir.glob('./models/*.rb').each { |r| require r}
 require "./db/seed"
 
-puts "There are #{Show.count} in the database"
+puts "Welcome! what day would you like to search for shows?
+====================================================="
+day_selected = gets.chomp
 
 Network.all.each do |network|
-	puts "Shows airing on #{network}"
-	network.shows.each do |show|
-		puts show
-	end	
+network.shows.where(day_of_week: day_selected).each do |show|
+	puts show
+	end
 end
+
+#Network.all.each do |network|
+#	puts "Shows airing on #{user_selection}"
+#	network.shows.each do |show|
+#		puts shows
+#	end	
+#end
