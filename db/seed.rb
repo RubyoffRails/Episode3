40a5346c1ac5cfc2ade62da381c3add5341e154f
Amitav Misra <amitav@gmail.com>
2012-05-30 14:42:05 -0500

    Episode3 Panda

--- db/seed.rb
+++ db/seed.rb
@@ -3,5 +3,8 @@ Network.delete_all
 Show.delete_all
 amc = Network.create(name: "AMC")
 nbc = Network.create(name: "NBC")
+hbo = Network.create(name: "HBO")
 Show.create(name: "Mad Men", day_of_week: "Sunday", hour_of_day: 22, network: amc)
 Show.create(name: "Community", day_of_week: "Thursday", hour_of_day: 20, network: nbc)
+Show.create(name: "Game of Thrones", day_of_week: "Sunday", hour_of_day: 21, network: hbo)
+Show.create(name: "30 Rock", day_of_week: "Thursday", hour_of_day: 2030, network: nbc)
--- watchman.rb
+++ watchman.rb
@@ -5,6 +5,12 @@ require "./db/setup"
 Dir.glob('./models/*').each { |r| require r}
 require "./db/seed"
 
+
+# Tiger level
+#puts "What day do you want to watch shows?"
+#Show.day_of_week = $stdin.gets
+
+
 puts "There are #{Show.count} in the database"
 
 Network.all.each do |network|
@@ -13,3 +19,10 @@ Network.all.each do |network|
 		puts show
 	end	
 end
+
+#Network.all.each do |day_of_week|
+#	puts "Shows airing on #{day_of_week}"
+#	network.shows.each do |show|
+#		puts show
+#	end	
+#end
