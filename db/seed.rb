d06d42ce408afb6a14912028f0632334001d9ae2
Anna Vester <veannaa@gmail.com>
2012-12-01 17:20:34 -0600

    Assignment #3: Panda and Tiger Levels

--- db/seed.rb
+++ db/seed.rb
@@ -3,5 +3,8 @@ Network.delete_all
 Show.delete_all
 amc = Network.create(name: "AMC")
 nbc = Network.create(name: "NBC")
+cbs = Network.create(name: "CBS")
 Show.create(name: "Mad Men", day_of_week: "Sunday", hour_of_day: 22, network: amc)
 Show.create(name: "Community", day_of_week: "Thursday", hour_of_day: 20, network: nbc)
+Show.create(name: "Person of Interest", day_of_week: "Thursday", hour_of_day: 20, network: cbs)
+Show.create(name: "Criminal Minds", day_of_week: "Wednesday", hour_of_day: 20, network: cbs)
--- watchman.rb
+++ watchman.rb
@@ -13,3 +13,16 @@ Network.all.each do |network|
 		puts show
 	end	
 end
+
+def find_show
+  dow = { Monday: 1, Tuesday: 2, Wednesday: 3, Thursday: 4, Friday: 5,  Saturday: 6, Sunday: 7 }
+  puts "Pick a day of week: Use 1 for Monday ... 7 for Sunday"
+  user_pick = dow.key ( gets.to_i )
+
+  Network.all.each do |network|
+    network.shows.each { |show| puts show if show.day_of_week == user_pick.to_s }
+  end
+
+end
+
+find_show
