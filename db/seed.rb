f2b31c455df8bc8be9fc012a99f4e9dded04ce3c
J. Michael Kasiewicz <crestcode@gmail.com>
2012-12-03 08:18:36 -0500

    Panda Level

--- db/seed.rb
+++ db/seed.rb
@@ -3,5 +3,9 @@ Network.delete_all
 Show.delete_all
 amc = Network.create(name: "AMC")
 nbc = Network.create(name: "NBC")
+abc = Network.create(name: "ABC")
+pbs = Network.create(name: "PBS")
 Show.create(name: "Mad Men", day_of_week: "Sunday", hour_of_day: 22, network: amc)
 Show.create(name: "Community", day_of_week: "Thursday", hour_of_day: 20, network: nbc)
+Show.create(name: "Modern Family", day_of_week: "Wednesday", hour_of_day: 21, network: abc)
+Show.create(name: "Downton Abbey", day_of_week: "Sunday:", hour_of_day: 21, network: pbs)
