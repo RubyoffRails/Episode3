7552c35dfeafc14f66dd0f81155816762f4846aa
J. Michael Kasiewicz <crestcode@gmail.com>
2012-12-03 09:00:10 -0500

    Tiger Level

--- watchman.rb
+++ watchman.rb
@@ -13,3 +13,18 @@ Network.all.each do |network|
 		puts show
 	end	
 end
+
+	puts "\nWhat day do you want to watch shows?"
+  user_day = gets.chomp()
+
+	shows_by_day = Show.where(day_of_week: user_day.capitalize)
+
+	if shows_by_day.size == 0
+		puts "\nSorry, no shows that day!"
+	else
+		puts "\nThere is #{shows_by_day.count} show on #{user_day.capitalize}:" if shows_by_day.size == 1
+		puts "\nThere are #{shows_by_day.count} shows on #{user_day.capitalize}:" if shows_by_day.size > 1
+		shows_by_day.each do |show_day|
+			puts show_day
+		end
+	end
