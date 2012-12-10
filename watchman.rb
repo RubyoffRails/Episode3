a5eeb2371692fe0986ea0d24588cb2c67ceb34b1
Ralphos <wintle.ralph@gmail.com>
2012-12-10 21:12:52 +0800

    Panda & Tiger level complete: can specify shows for a specific day

--- watchman.rb
+++ watchman.rb
@@ -6,10 +6,14 @@ Dir.glob('./models/*').each { |r| require r}
 require "./db/seed"
 
 puts "There are #{Show.count} in the database"
+puts 
+puts "What day would you like to watch shows?"
+puts 
+print '~>'
+day = gets.chomp.capitalize
 
 Network.all.each do |network|
-	puts "Shows airing on #{network}"
-	network.shows.each do |show|
-		puts show
-	end	
+  network.shows.where(day_of_week: day).each do |show|
+    puts show 
+  end	
 end
