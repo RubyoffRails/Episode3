04d14fcee99b8053769bd4067d4d49cc48194d30
leToaster <s.media.me@gmail.com>
2012-12-12 23:20:09 -0500

    Eagle Tiger Panda

--- db/seed.rb
+++ db/seed.rb
@@ -11,3 +11,13 @@ Show.create(name: "The Dog Whisperer", day_of_week: "Friday", hour_of_day: 10, n
 Show.create(name: "My Cat From Hell", day_of_week: "Saturday", hour_of_day: 10, network: nat)
 Show.create(name: "Battlestar Galactica", day_of_week: "Saturday", hour_of_day: 11, network: sci)
 Show.create(name: "Dr. Who", day_of_week: "Saturday", hour_of_day: 13, network: sci)
+
+Book.create(author: "Fyodor Dostoyevsky",title: "The Brothers karamazov", rating: 10)
+Book.create(author: "Fyodor Dostoyevsky",title: "Crime And Punishment", rating: 10)
+Book.create(author: "Charles Bukowski",title: "Ham On Rye", rating: 10)
+Book.create(author: "Alexander Solzhenitsyn",title: "Cancer Ward", rating: 7)
+Book.create(author: "China Mieville", title: "The Scar", rating: 9)
+Book.create(author: "Colson Whitehead", title: "John Henry Days", rating: 3)
+Book.create(author: "Junot Diaz", title: "The Brief Wondrous Life of Oscar Wao", rating: 8)
+Book.create(author: "Richard Wright", title: "The Outsider", rating: 8)
+Book.create(author: "Richard Wright", title: "Native Son", rating: 9)
--- watchman.rb
+++ watchman.rb
@@ -1,15 +1,26 @@
 require 'rubygems'
 require 'bundler/setup'
 require "./db/setup"
-
 Dir.glob('./models/*').each { |r| require r}
 require "./db/seed"
 
 puts "What day do you want to watch tv?"
 user_input = gets.chomp
 
-Show.all.each  do  |s|
-  if user_input.casecmp(s.day_of_week.to_s) == 0
-    puts s
+Show.all.each  do  |show|
+  if  user_input.casecmp(show.day_of_week) == 0
+    puts show
   end
 end
+
+2.times { puts "\n" }
+
+puts "Here is a partial list of books I own"
+puts Book.all
+2.times { puts "\n" }
+
+puts "What author or book would you like to know more about?"
+user_input = gets.chomp
+
+book = Book.where(author: user_input).all
+puts book
