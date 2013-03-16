2d853f44da4029e5c1fb49fe3cbab3c417dab7f2
meltar <meltar@gmail.com>
2013-03-16 19:02:19 -0400

    Tiger complete

--- watchman.rb
+++ watchman.rb
@@ -19,7 +19,7 @@ Show.all.each do |show|
 end
 
 puts "\nWhat day do you want to watch a show?"
-day = gets.chomp()
+day = gets.chomp().capitalize
 puts "\nShows on #{day}"
 Show.all.each do |show| 
 	puts show if show.day_of_week == day
