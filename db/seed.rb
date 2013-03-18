2a8dc1da2ca72343ecdf79dd5630c554f65498f8
Darron Froese <darron@froese.org>
2013-03-17 19:39:36 -0600

    Tiger level.

--- db/seed.rb
+++ db/seed.rb
@@ -7,3 +7,4 @@ Show.create(name: "Mad Men", day_of_week: "Sunday", hour_of_day: 22, network: am
 Show.create(name: "Breaking Bad", day_of_week: "Friday", hour_of_day: 20, network: amc)
 Show.create(name: "Community", day_of_week: "Thursday", hour_of_day: 20, network: nbc)
 Show.create(name: "Whitney", day_of_week: "Wednesday", hour_of_day: 20, network: nbc)
+Show.create(name: "FNL", day_of_week: "Friday", hour_of_day: 21, network: nbc)
--- watchman.rb
+++ watchman.rb
@@ -5,11 +5,25 @@ require "./db/setup"
 Dir.glob('./models/*').each { |r| require r}
 require "./db/seed"
 
-puts "There are #{Show.count} in the database"
+puts "There are #{Show.count} in the database.\n"
 
 Network.all.each do |network|
-	puts "Shows airing on #{network}"
-	network.shows.each do |show|
-		puts show
-	end	
+  puts "\nShows airing on #{network}"
+  network.shows.each do |show|
+    puts show
+  end 
+end
+
+puts "\nWhat day would you like to watch TV?"
+day = gets
+
+shows = Show.where("day_of_week = ?", day.chomp)
+
+unless shows.empty?
+  puts "Shows on #{day.chomp}:"
+  shows.each do |show|
+    puts show.to_s
+  end
+else
+  puts "No shows on that day"
 end
