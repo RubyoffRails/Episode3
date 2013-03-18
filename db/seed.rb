860dfdc706639ae3a151af88fcf53ad711323668
Darron Froese <darron@froese.org>
2013-03-17 19:13:05 -0600

    Panda level.

--- db/seed.rb
+++ db/seed.rb
@@ -4,4 +4,6 @@ Show.delete_all
 amc = Network.create(name: "AMC")
 nbc = Network.create(name: "NBC")
 Show.create(name: "Mad Men", day_of_week: "Sunday", hour_of_day: 22, network: amc)
+Show.create(name: "Breaking Bad", day_of_week: "Friday", hour_of_day: 20, network: amc)
 Show.create(name: "Community", day_of_week: "Thursday", hour_of_day: 20, network: nbc)
+Show.create(name: "Whitney", day_of_week: "Wednesday", hour_of_day: 20, network: nbc)
