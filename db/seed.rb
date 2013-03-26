29fceb47a7ed33dd3fdad70c14129715c44ffa05
dharini <dramanat459@gmail.com>
2013-03-25 22:30:05 -0500

    panda and tiger level

--- db/seed.rb
+++ db/seed.rb
@@ -3,5 +3,9 @@ Network.delete_all
 Show.delete_all
 amc = Network.create(name: "AMC")
 nbc = Network.create(name: "NBC")
+cbs = Network.create(name: "CBS")
 Show.create(name: "Mad Men", day_of_week: "Sunday", hour_of_day: 22, network: amc)
 Show.create(name: "Community", day_of_week: "Thursday", hour_of_day: 20, network: nbc)
+Show.create(name: "60 Minutes", day_of_week: "Sunday", hour_of_day: 18, network: cbs)
+Show.create(name: "Survivor", day_of_week: "Thursday", hour_of_day: 20, network: cbs)
+
--- watchman.rb
+++ watchman.rb
@@ -6,10 +6,18 @@ Dir.glob('./models/*').each { |r| require r}
 require "./db/seed"
 
 puts "There are #{Show.count} in the database"
+puts "What day of the week would you like to watch a show?"
+day_of_week = gets.chomp
 
+count = 0
 Network.all.each do |network|
-	puts "Shows airing on #{network}"
 	network.shows.each do |show|
-		puts show
-	end	
+		if show.day_of_week == day_of_week
+		  puts "#{show} on #{network}"
+		  count = count + 1
+		end
+	end
+end
+if count == 0
+	puts "no shows air on #{day_of_week}"
 end
