916da2c4a455de2f86069e768aebe0a449e030e1
ardder <fang.hsiente@gmail.com>
2013-07-15 13:16:30 +0800

    tiger level completed

--- watchman.rb
+++ watchman.rb
@@ -2,14 +2,48 @@ require 'rubygems'
 require 'bundler/setup'
 
 require "./db/setup"
-Dir.glob('./models/*').each { |r| require r}
+Dir.glob('./models/*').each { |r| require r }
 require "./db/seed"
 
-puts "There are #{Show.count} in the database"
+WEEK_DAY = {
+  '1' => "Monday",
+  '2' => "Tuesday",
+  '3' => "Wednesday",
+  '4' => "Thursday",
+  '5' => "Friday",
+  '6' => "Saturday",
+  '7' => "Sunday"
+}
 
-Network.all.each do |network|
-	puts "Shows airing on #{network}"
-	network.shows.each do |show|
-		puts show
-	end	
+def select_day
+  puts "-" * 80
+  puts "Which week day do you want watch TV shows?"
+  puts "1: Monday, 2: Tuesday, 3: Wednesday, 4: Thursday, 5: Friday, 6: Saturday, 7: Sunday or Q: Quit"
+  STDOUT.write "Select a day [1~7 or Q]:"
+  gets.chomp
+end
+
+while true
+  day = select_day
+
+  if day.upcase == 'Q'
+    puts "Goodbye!"
+    break
+  end
+
+  unless day.match(/^[1-7]$/)
+    puts "No such day: '#{day}', please select again!"
+    next
+  end
+
+  sday = WEEK_DAY[day]
+  shows = Show.where(day_of_week: sday)
+
+  if shows.empty?
+    puts "No shows on #{sday}"
+  else
+    puts "Shows on #{sday}:"
+    shows.each {|show| puts show}
+  end
+
 end
