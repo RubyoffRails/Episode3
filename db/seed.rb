15edf6ad42f53dea4ece06389f76d32909010ed0
noahpatterson <noah@noah-patterson.com>
2013-12-31 18:44:47 -0500

    Panda level. Add 2 more shows.

--- db/seed.rb
+++ db/seed.rb
@@ -1,7 +1,11 @@
 # Cleaning Out
 Network.delete_all
 Show.delete_all
+
 amc = Network.create(name: "AMC")
 nbc = Network.create(name: "NBC")
+showtime = Network.create(name: "Showtime")
 Show.create(name: "Mad Men", day_of_week: "Sunday", hour_of_day: 22, network: amc)
 Show.create(name: "Community", day_of_week: "Thursday", hour_of_day: 20, network: nbc)
+Show.create(name: "Hannibal", day_of_week: "Monday", hour_of_day: 20, network: nbc)
+Show.create(name: "Homeland", day_of_week: "Thursday", hour_of_day: 20, network: showtime)
