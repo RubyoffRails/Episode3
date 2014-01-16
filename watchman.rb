2a29cff437f62033a4b52fd765559c8ab03d9cd1
jason perez <jperezish@gmail.com>
2014-01-15 17:37:51 -0800

    add CLI to ask for day of week, app returns shows that match the provided day

--- watchman.rb
+++ watchman.rb
@@ -5,13 +5,18 @@ require "./db/setup"
 Dir.glob('./models/*').each { |r| require r}
 require "./db/seed"
 
+puts "***********************"
+puts "* Welcome to WATCHMAN *"
+puts "***********************"
 puts "There are #{Show.count} in the database"
+puts ""
+puts "What day would you like to watch shows?"
+print "> "
+
+day_of_week = gets.chomp.downcase
 
 Network.all.each do |network|
-  puts "---------------------------"
-	puts "Shows airing on #{network}:"
-	network.shows.each do |show|
-		puts show
-	end
-  puts ""
+  network.shows.each do |show|
+    puts show if show.day_of_week.downcase == day_of_week
+  end
 end
