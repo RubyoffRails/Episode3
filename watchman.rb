f3bd867f8060131bf0d6be7fbe2e59067275924a
jason perez <jperezish@gmail.com>
2014-01-22 23:37:26 -0800

    update to use DB where query

--- watchman.rb
+++ watchman.rb
@@ -5,22 +5,19 @@ require "./db/setup"
 Dir.glob('./models/*').each { |r| require r}
 require "./db/seed"
 
-i=0
-
 puts "***********************"
 puts "* Welcome to WATCHMAN *"
 puts "***********************"
 puts "There are #{Review.count} reviews in the database:"
 puts ""
 Review.all.each do |review|
-  i+=1
-  puts "#{review.movie}"
+  puts review.movie
 end
 puts "What movie review would you like to see?"
 print "> "
 
-movie_selection = gets.chomp.downcase
+movie_selection = gets.chomp
 
-my_selection = Review.all.select { |review| review.movie.downcase == movie_selection }
+my_selection = Review.where(movie: movie_selection)
 puts "Oops... we don't have a review for #{movie_selection} yet." if my_selection.empty?
 my_selection.each { |review| puts review }
