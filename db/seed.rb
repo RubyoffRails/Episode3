e98ecd6df900f716d5421d325657700446e6c10d
drammopo <drammopo@gmail.com>
2014-02-18 02:49:54 -0800

    Moved Beer.delete_all closer to clean-out section of seed.rb.

--- db/seed.rb
+++ db/seed.rb
@@ -1,6 +1,7 @@
 # Cleaning Out
 Network.delete_all
 Show.delete_all
+Beer.delete_all
 
 amc = Network.create(name: "AMC")
 nbc = Network.create(name: "NBC")
@@ -16,7 +17,6 @@ Show.create(name: "Suits", day_of_week: "Thursday", hour_of_day: 21, network: us
 Show.create(name: "House of Cards", day_of_week: "Friday", hour_of_day: 20, network: netflix)
 Show.create(name: "House of Lies", day_of_week: "Sunday", hour_of_day: 22, network: sho)
 
-Beer.delete_all
 Beer.create(name: 'Nova Schin', country: 'Brazil', brewer: ' Brasil Kirin', kind_of: 'Pilsner', rating: 5, summary:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed')
 Beer.create(name: 'Tusker', country: 'Kenya', brewer: 'East African Breweries', kind_of: 'Lager', rating: 4, summary: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ...')
 Beer.create(name: 'Tui', country: 'New Zealand', brewer: 'DB Breweries', kind_of: 'Indian Pale Ale', rating: 4, summary: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ...')
