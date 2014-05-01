f64950515c2737477ac4cae9ed4ea335209a62a8
PDaily <pat.irwin4@gmail.com>
2014-05-01 01:10:53 +0000

    Tiger level. Added 2 shows, and request a day to search from user.

--- db/seed.rb
+++ db/seed.rb
@@ -3,5 +3,9 @@ Network.delete_all
 Show.delete_all
 amc = Network.create(name: "AMC")
 nbc = Network.create(name: "NBC")
+fx = Network.create(name: "FX")
+bbca = Network.create(name: "BBC America")
 Show.create(name: "Mad Men", day_of_week: "Sunday", hour_of_day: 22, network: amc)
 Show.create(name: "Community", day_of_week: "Thursday", hour_of_day: 20, network: nbc)
+Show.create(name: "Archer", day_of_week: "Monday", hour_of_day: 19 , network: fx)
+Show.create(name: "Dr. Who", day_of_week: "Wednesday", hour_of_day: 20, network: bbca)
--- watchman.rb
+++ watchman.rb
@@ -1,15 +1,20 @@
 require 'rubygems'
 require 'bundler/setup'
 
-require "./db/setup"
+require './db/setup'
 Dir.glob('./models/*').each { |r| require r}
-require "./db/seed"
+require './db/seed'
 
 puts "There are #{Show.count} in the database"
 
-Network.all.each do |network|
-	puts "Shows airing on #{network}"
-	network.shows.each do |show|
-		puts show
-	end	
+puts 'What day would you like to search?'
+day = gets.strip.capitalize
+results = Show.where(day_of_week: day)
+if results == []
+  puts "There are no shows on #{day}"
+else
+  puts "These are the shows that are playing on #{day}"
+  results.each do |show|
+    puts show
+  end
 end
